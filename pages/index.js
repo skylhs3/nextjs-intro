711a125d1bc63eef9b5e3a80e39b394ee5b2ce0b
i hyonson <skylhs3@gmail.com>
2023-12-10 10:55:57 +0900

    #2.6 Movie Detail

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,50 @@
 import NavBar from "@/components/NavBar";
 import Head from "next/head";
 import Seo from "@/components/Seo";
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
-export default function Home({results}) {
+export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push(
+      {
+        pathname: `/movies/${id}`,
+        query: {
+          id,
+          title,
+        },
+      },
+      `/movies/${id}`
+    );
+  };
   return (
     <div className="container">
       <Seo title="Home" />
-      {results?.map( (movie) => (
-        <div className="movie" key={movie.id}>
+      {results?.map((movie) => (
+        <div
+          onClick={() => onClick(movie.id, movie.original_title)}
+          className="movie"
+          key={movie.id}
+        >
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+          <h4>
+            <Link
+              href={{
+                pathname: `/movies/${movie.id}`,
+                query: {
+                  id:movie.id,
+                  title: movie.original_title,
+                },
+              }}
+              as={`/movies/${movie.id}`}
+              key={movie.id}
+              legacyBehavior
+            >
+              <a>{movie.original_title}</a>
+            </Link>
+          </h4>
         </div>
       ))}
 
@@ -34,18 +68,22 @@ export default function Home({results}) {
           font-size: 18px;
           text-align: center;
         }
+        .movie {
+          cursor: pointer;
+        }
       `}</style>
     </div>
   );
 }
 
-export async function getServerSideProps(){
-  const { results } = await (await fetch(`http://localhost:3000/api/movies`)).json();
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
 
   return {
     props: {
       results,
-    }
-  }
-
-} 
+    },
+  };
+}
